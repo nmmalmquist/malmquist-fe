4c472a9fbb59e4a29a52c0d458942f41f8defb63
Nick Malmquist <nicholasmmalmquist@gmail.com>
2024-04-04 07:55:36 -0500

    fixed build error

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -1,6 +1,5 @@
 <script>
 	import Navbar from '$lib/components/Navbar.svelte';
-	import { NavItemEnum } from '$lib/types/NavItem';
 </script>
 
-<Navbar selectedIndex={NavItemEnum.PHOTOS} />
+<Navbar />
