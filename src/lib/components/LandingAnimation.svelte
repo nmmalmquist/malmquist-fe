e583581e44f2906ee1db34db1417254d8c6ce2f4
Nick Malmquist <nicholas.malmquist@fbitn.com>
2024-04-05 17:21:41 -0500

    more loading animation

--- src/lib/components/LandingAnimation.svelte
+++ src/lib/components/LandingAnimation.svelte
@@ -5,19 +5,51 @@
 </script>
 
 <div
-	class="bg-dark fixed top-0 left-0 w-full h-screen z-10 landing-animation flex justify-center items-center text-white"
+	class={twMerge(
+		'bg-dark fixed top-0 left-0 w-full h-screen z-10 flex justify-center items-center text-white',
+		displayText ? 'landing-animation-navigate' : 'landing-animation-intro'
+	)}
 >
-	<span class={twMerge('text-4xl', !displayText ? 'spin-through-text' : '')}>
+	<!-- <div class="absolute bg-dark h-0 rounded-full w-[150%] round-height-animation"></div> -->
+	<span class={twMerge('text-4xl', !displayText ? 'spin-through-text' : 'display-text-appear')}>
 		{displayText}
 	</span>
 </div>
 
 <style lang="postcss">
+	@keyframes display-text {
+		0% {
+			opacity: 0;
+		}
+		50% {
+			opacity: 1;
+		}
+		70% {
+			opacity: 1;
+		}
+		100% {
+			opacity: 0;
+		}
+	}
 	@keyframes slide-up {
 		0% {
 			top: 0%;
 		}
-		50% {
+		80% {
+			top: 0%;
+		}
+		100% {
+			top: -100%;
+		}
+	}
+	@keyframes slide-up-from-down {
+		0% {
+			top: 100%;
+		}
+		30% {
+			top: 0%;
+		}
+		70% {
 			top: 0%;
 		}
 		100% {
@@ -26,22 +58,74 @@
 	}
 	@keyframes change-text {
 		0% {
-			content: 'hello.';
+			content: 'Hello';
+			opacity: 0;
+		}
+		10% {
+			opacity: 0;
+		}
+		30% {
+			content: 'Hello';
+			opacity: 1;
+		}
+		40% {
+			content: 'Ciao';
+			opacity: 1;
+		}
+		45% {
+			content: 'שלום';
 		}
 		50% {
-			content: 'yo';
+			content: 'Aloha';
+		}
+		55% {
+			content: '你好';
+		}
+		60% {
+			content: 'Hola';
+		}
+		65% {
+			content: 'नमस्ते';
+		}
+
+		70% {
+			content: 'Привет';
+		}
+		75% {
+			content: 'မင်္ဂလာပါ';
 		}
 		80% {
-			content: 'hola';
+			content: 'こんにちは';
+		}
+		85% {
+			content: 'Bonjour';
+		}
+		90% {
+			content: 'Cześć';
+		}
+		95% {
+			content: 'Hello';
+		}
+		100% {
+			content: 'Hello';
+			opacity: 1;
 		}
 	}
-	.landing-animation {
-		animation: slide-up 4s forwards;
-		content: 'asdfafd';
+	.landing-animation-intro {
+		animation: slide-up 3s forwards;
 	}
+	.landing-animation-navigate {
+		animation: slide-up-from-down 2s forwards ease-in-out;
+	}
+
 	.spin-through-text::after {
-		content: 'hello.';
+		content: 'Hello';
 		font-size: theme(fontSize.4xl);
-		animation: change-text 1s;
+		animation: change-text 2s forwards;
+		opacity: 0;
+	}
+	.display-text-appear {
+		opacity: 0;
+		animation: display-text 2s ease-in-out;
 	}
 </style>
