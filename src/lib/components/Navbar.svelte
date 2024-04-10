bae9fd68f758290a9fa6ccc981a46c66b4a89170
Nick Malmquist <nicholas.malmquist@fbitn.com>
2024-04-10 13:18:05 -0500

    added overflow-hidden

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -23,27 +23,32 @@
 		}
 	];
 
-	let mobileDrawerOpen = false
+	let mobileDrawerOpen = false;
 </script>
 
 <nav class="w-full absolute text-white font-semibold">
 	<div class="relative">
-
 		<div class="flex flex-row justify-between p-8">
 			<div class="p-4">
-				<a href="/" class={twMerge("group", activeLink === NavItemEnum.NONE && "cursor-default")}>
-					<div class="flex ">
-						<div class="block credit transition-transform ease-in-out sm:group-hover:rotate-[360deg] duration-500">
+				<a href="/" class={twMerge('group', activeLink === NavItemEnum.NONE && 'cursor-default')}>
+					<div class="flex">
+						<div
+							class="block credit transition-transform ease-in-out sm:group-hover:rotate-[360deg] duration-500"
+						>
 							<span>©</span>
 						</div>
 						<div class="overflow-hidden w-[6.5rem] sm:group-hover:w-60 duration-500">
-							<div class="pl-1 transition-transform duration-500 ease-in-out sm:group-hover:translate-x-[-4.2rem]  whitespace-nowrap"><span>Code by</span> Nick Malmquist | Rock and Roll</div>
+							<div
+								class="pl-1 transition-transform duration-500 ease-in-out sm:group-hover:translate-x-[-4.2rem] whitespace-nowrap"
+							>
+								<span>Code by</span> Nick Malmquist | Rock and Roll
+							</div>
 						</div>
 					</div>
 				</a>
 			</div>
 			<div class="relative p-4 bullet sm:hidden flex justify-center">
-				<button on:click={() => mobileDrawerOpen = !mobileDrawerOpen}>Menu</button>
+				<button on:click={() => (mobileDrawerOpen = !mobileDrawerOpen)}>Menu</button>
 			</div>
 			<ul class="hidden sm:flex flex-row gap-10 items-center">
 				{#each navItems as item}
@@ -53,8 +58,16 @@
 				{/each}
 			</ul>
 		</div>
-		<div class={twMerge("sm:hidden h-screen w-full p-4 bg-dark absolute top-0 right-0 translate-x-[100%] transition-transform duration-1000 ease-in-out", mobileDrawerOpen && "nav-drawer-active")}>
-			<button class="bg-primary h-20 w-20 rounded-full fixed top-4 right-4" on:click={()=> mobileDrawerOpen = false}>X</button>
+		<div
+			class={twMerge(
+				'sm:hidden h-screen w-full p-4 bg-dark absolute top-0 right-0 translate-x-[100%] transition-transform duration-1000 ease-in-out',
+				mobileDrawerOpen && 'nav-drawer-active'
+			)}
+		>
+			<button
+				class="bg-primary h-20 w-20 rounded-full fixed top-4 right-4"
+				on:click={() => (mobileDrawerOpen = false)}>X</button
+			>
 		</div>
 	</div>
 </nav>
@@ -85,5 +98,4 @@
 	.nav-drawer-active {
 		transform: translateX(0%);
 	}
-
 </style>
