27834d3eeb6a616d771584a70372f48991534ac9
Nick Malmquist <nicholasmmalmquist@gmail.com>
2024-04-11 21:42:35 -0500

    some front page work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,28 +7,39 @@
 <LandingAnimation />
 <Navbar activeLink={NavItemEnum.HOME} />
 <main>
-	<section class="bg-gray w-screen h-screen flex flex-col justify-center">
-		<div class="w-full flex justify-between items-center">
-			<div class="bg-dark w-64 h-28 rounded-r-full"></div>
-			<div class="pr-28 flex items-center gap-2">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width={1.5}
-					stroke="currentColor"
-					class="w-10 h-10 text-white"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M14.25 9.75 16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z"
-					/>
-				</svg>
+	<section class="bg-gray w-screen h-screen flex justify-center flex-col-reverse md:flex-col">
+		<div class="w-full flex justify-between items-end md:flex-1">
+			<div class="flex items-center justify-center md:justify-between w-full mb-7 md:m-0">
+				<div class="bg-dark w-64 h-28 rounded-r-full hidden md:block"></div>
+				<div class="md:pr-[8%] flex items-center gap-2">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width={1.5}
+						stroke="currentColor"
+						class="w-10 h-10 text-white"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M14.25 9.75 16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z"
+						/>
+					</svg>
 
-				<h2 class="text-white text-3xl">Software Engineer</h2>
+					<h2 class="text-white text-3xl">Software Engineer</h2>
+				</div>
 			</div>
+		</div>
+		<div class="flex flex-1 items-end justify-center">
+			<h1 class=" whitespace-nowrap my-10 text-white title-name">Nick Malmquist</h1>
 		</div>
 	</section>
 	<section class="bg-dark w-screen h-screen flex justify-center"></section>
 </main>
+
+<style>
+	.title-name {
+		font-size: max(9em, 15vw);
+	}
+</style>
