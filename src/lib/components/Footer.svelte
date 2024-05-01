7d1e209f67fcb2b67d89efaa139af6d94639a5e2
Nick Malmquist <nicholas.malmquist@fbitn.com>
2024-05-01 08:07:12 -0500

    footer fixes

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,14 +2,15 @@
 </script>
 
 <footer
-	class="bg-dark text-secondary py-20 px-10 md:px-[10%] xl:px-[20%] w-full h-screen overflow-hidden"
+	class="bg-dark text-secondary py-20 px-4 md:px-[10%] xl:px-[20%] w-full h-screen overflow-hidden"
 	data-scroll-section
 	data-scroll-section-id="footer"
 >
 	<div data-scroll data-scroll-speed="-4" class="flex flex-col gap-24">
-		<div class="flex flex-col md:flex-row md:gap-32 gap-6">
-			<h2 class="text-8xl flex-[3] self-center text-center md:text-start">Let's Work Together!</h2>
-		</div>
+		<h2 class="text-6xl sm:text-8xl flex-[3] self-center text-center md:text-start">
+			Let's Work Together!
+		</h2>
+
 		<div class="relative w-full flex justify-end items-center">
 			<hr class="bg-gray w-full absolute" />
 			<div
@@ -28,14 +29,12 @@
 				</div>
 			</div>
 		</div>
-		<div
-			class="flex gap-10 flex-col md:flex-row justify-center md:justify-between items-center flex-wrap"
-		>
-			<div class="flex gap-10 flex-col md:flex-row">
-				<div class="text-xl border-gray border-2 py-6 px-10 rounded-full flex-shrink-0">
+		<div class="flex gap-10 flex-col md:flex-row justify-center md:justify-between items-center">
+			<div class="flex gap-10 flex-col md:flex-row flex-wrap">
+				<div class="text-md border-gray border-2 py-2 px-6 rounded-full text-center flex-shrink-0">
 					+1 (270) 779-1464
 				</div>
-				<div class="text-xl border-gray border-2 py-6 px-10 rounded-full flex-shrink-0">
+				<div class="text-md border-gray border-2 py-2 px-6 rounded-full text-center flex-shrink-0">
 					nicholasmmalmquist@gmail.com
 				</div>
 			</div>
